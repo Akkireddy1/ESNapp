01fa837ac890b1548041ba96022a7abfe79bf2c0
Demijan <demi.lesjak@gmail.com>
2017-01-23 23:03:15 +0100

    firebase displays  events

--- src/pages/events/events.ts
+++ src/pages/events/events.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import {AngularFire, FirebaseListObservable} from 'angularfire2';
 
 /*
   Generated class for the Events page.
@@ -13,7 +14,11 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class EventsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  events: FirebaseListObservable<any>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, af: AngularFire) {
+    this.events=af.database.list('/events');
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EventsPage');

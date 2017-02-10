7a3cc50a14c8e5b61221f4439aa6cdf51862c062
Demijan <demi.lesjak@gmail.com>
2017-02-10 21:16:05 +0100

    map init with custom markers work

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { MapProvider } from '../../providers/map-provider';
 
+declare var google;
 
 @Component({
   selector: 'page-map',
@@ -11,13 +12,37 @@ export class MapPage {
 
   @ViewChild('map') mapElement;
   map: any;
+  
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,public mapService: MapProvider) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams,public mapService: MapProvider) {
+    
+  }
 
 /// TODO SPREMENI IN PREMAKNI V PROVIDERJA
   ionViewDidLoad() {
     console.log('ionViewDidLoad MapPage');
-    this.map=this.mapService.initMap(this.mapElement);
+    this.map=this.mapService.initMap(this.mapElement, this.loadLocations());
+  }
+
+
+  //get all google locations for creating a markers
+  loadLocations(){
+    var locations=[
+      {
+      position:new google.maps.LatLng(46.554650, 15.645881),
+      type:'restaurant'
+    },
+    {
+      position:new google.maps.LatLng(46.5584864, 15.6449621),
+      type:'restaurant'
+    },
+    {
+      position:new google.maps.LatLng(46.5628445, 15.6298367),
+      type:'restaurant'
+    }
+    ]
+    console.log("locations loaded: "+locations[0].position);
+    return locations;
   }
 
 }
--- src/providers/map-provider.ts
+++ src/providers/map-provider.ts
@@ -12,21 +12,65 @@ declare var google;
 @Injectable()
 export class MapProvider {
 
-  map:any;
+  map: any;
+  markers: any[] = [];
 
   constructor(public http: Http) {
     console.log('Hello MapProvider Provider');
   }
 
 
-  initMap(mapElement: any){
+  initMap(mapElement: any, locations: any[]) {
+    //Maribor latlng setting
     let latlng = new google.maps.LatLng(46.554650, 15.645881);
 
     let mapOptions = {
-        center:latlng,
-        zoom:13
+      center: latlng,
+      zoom: 13
     };
     this.map = new google.maps.Map(mapElement.nativeElement, mapOptions);
-    return this.map
+    if (locations.length) {
+      console.log("locations not empy");
+      this.createMarkers(locations, "");
+    }
+    this.showMarkers(this.markers);
+    this.setMarkerIcon(this.markers, "http://megaicons.net/static/img/icons_sizes/8/178/32/catering-restaurant-icon.png");
+    return this.map;
   }
+
+  //if picUrl is empty it shows normal icon
+  createMarkers(locations: any[], picUrl: String) {
+    for (let location of locations) {
+      let marker = new google.maps.Marker({
+        position: location.position,
+        title: 'Your favorite city!'
+      });
+      this.markers.push(marker);
+
+      if (picUrl != "" || picUrl != undefined) {
+        console.log("picUrl not empty")
+        marker.setIcon(picUrl);
+      }
+    }
+
+
+
+  }
+
+  showMarkers(markers: any[]) {
+    for (let marker of markers) {
+      marker.setMap(this.map);
+    }
+  }
+  hideMarkers(markers: any[]) {
+    for (let marker of markers) {
+      marker.setMap(null);
+    }
+  }
+  setMarkerIcon(markers: any[], picUrl: String) {
+    for (let marker of markers) {
+      marker.setIcon(picUrl)
+    }
+  }
+
 }
